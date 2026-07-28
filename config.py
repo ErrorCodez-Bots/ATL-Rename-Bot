b9e252df3e59d950a083733d0072db487416413f
ErrorCodez Bots <147271056+ErrorCodez-Bots@users.noreply.github.com>
2026-07-29 01:47:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ API_HASH = os.environ.get("API_HASH", "4aef7d9e065d92f4a95736eaeb93d3ac")
 #BOT_TOKEN = os.environ.get("BOT_TOKEN", "8605444314:AAHZxA7RHBZxBIzUPFlQFQiRYk16a77zldg") 
 TOKEN_ONE = os.environ.get("TOKEN_ONE", "8605444314:AAHZxA7RHBZxBIzUPFlQFQiRYk16a77zldg")
 
-CHANNEL = os.environ.get("CHANNEL", "ATL_Univers") # username without '@'
+CHANNEL = os.environ.get("CHANNEL", "Tamil_Dubbed_Anime_Series") # username without '@'
 BOT_USERNAME = os.environ.get("BOT_USERNAME","XRenamerPro_Bot") # username without '@'
-SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP","BYNF_TamilChat") # username without '@'
-UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","ATL_Univers") # username without '@'
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME","HMF_Owner_1")
+SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP","anime_Hub_Team") # username without '@'
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","Tamil_Dubbed_Anime_Series") # username without '@'
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME","ZxZenitsu")
 STRING = os.environ.get("STRING", "BQE1pmMAU2rFVha4YI8H1Wi9agAYXDeiBTgcq5UQ_tUeFqBXMf9elvU505O4hMoPf25MgQ9S08IiI-U0S7Y0uz6r0B7_yx6VIrtehllyrydYdnYrIDDc6u-EGDzRg1gpyPUy7Qg1M3a0A80Yn39nce_W3HsmwT5GcUr3uB2NUbIu4B3W67olI5j780OrFwjBh_aAWHf11uSMBkqeGdNbxMmJK3rG4cA8-TxPiMAJsYcI6S2yvq-5Y4jdqz7IUizFVvTI6K_SLWcqU91PxQc8oIAyNx7_GznAX8xmNgyvmMwPmbum5xBVlQB0U85223ChDgNRuFtypIgWVmDMlNNxLfLYg9gmDQAAAAGXWVHVAA")
 
 DB_NAME = os.environ.get("DB_NAME","renameone")     
